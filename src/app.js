0b7f8d666169b7843bef3da605eb2e7bac1104f3
Ankur-Harshit <ankurharshit1@gmail.com>
2025-04-05 18:22:45 +0530

    Lec 4 updated

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const app = express();
 
+
+app.get("/user", (req, res)=>{
+    res.send("Hello got the get Request");
+})
+
+app.post("/user", (req, res)=>{
+    // post the data -> write the logic
+    res.send("Posted the data successuly on the DB");
+})
+
+app.delete("/user", (req, res)=>{
+    res.send("Deleted the data Successfuly");
+})
+
 app.use("/hello", (req, res)=>{
     res.send("Hello Hello Hello Amla Toli Durga Puja Samiti");
 })
 
-app.use("/test", (req, res)=>{
-    res.send("Hello from the Testing Server");
-})
+// app.use("/test", (req, res)=>{
+//     res.send("Hello from the Testing Server");
+// })
 
-app.use("/", (req, res)=>{
-    res.send("Hello from the Server");
-})
+// app.use("/", (req, res)=>{
+//     res.send("Hello from the Server");
+// })
 
 
 app.listen(3000, ()=>{
